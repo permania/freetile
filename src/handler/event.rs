2ef6646b6ad70b9662cfd7df487670a30144c876
permania <0onesixed@gmail.com>
2026-05-11 14:54:01 -0700

    fix: make event loop error-tolerant

--- src/handler/event.rs
+++ src/handler/event.rs
@@ -12,78 +12,99 @@ use super::wm::{WM, focus_and_warp, focus_window, is_mapped, retile};
 
 pub fn event_loop(wm: &mut WM) {
     loop {
-        match wm.conn.wait_for_event().unwrap() {
-            Event::Error(e) => eprintln!("error: {:?}", e),
-            Event::MapRequest(e) => {
-                let window = e.window;
+        match wm.conn.wait_for_event() {
+            Ok(event) => match event {
+                Event::Error(e) => eprintln!("error: {:?}", e),
+                Event::MapRequest(e) => {
+                    let window = e.window;
 
-                wm.state.windows_mut().push(window);
+                    let Some(attrs) = wm
+                        .conn
+                        .get_window_attributes(e.window)
+                        .ok()
+                        .and_then(|c| c.reply().ok())
+                    else {
+                        continue;
+                    };
 
-                wm.conn
-                    .change_window_attributes(
-                        window,
-                        &ChangeWindowAttributesAux::new().event_mask(EventMask::ENTER_WINDOW),
-                    )
-                    .unwrap();
+                    if attrs.override_redirect {
+                        continue;
+                    }
 
-                let prev_focused = wm.state.focused();
+                    if !wm.state.windows().contains(&window) {
+                        wm.state.windows_mut().push(window);
+                    }
 
-                let intent = retile(wm);
-                map_intent(wm, intent);
+                    wm.conn
+                        .change_window_attributes(
+                            window,
+                            &ChangeWindowAttributesAux::new().event_mask(EventMask::ENTER_WINDOW),
+                        )
+                        .unwrap();
 
-                if is_mapped(wm, window) {
-                    focus_and_warp(wm, window);
-                } else if let Some(p) = prev_focused {
-                    focus_and_warp(wm, p);
-                }
+                    let prev_focused = wm.state.focused();
 
-                wm.conn.flush().unwrap();
-            }
-            Event::UnmapNotify(e) => {
-                if !wm.state.windows().contains(&e.window) {
-                    continue;
-                }
+                    let intent = retile(wm);
+                    map_intent(wm, intent);
 
-                if wm.ignore_unmaps > 0 {
-                    eprintln!("ignore unmaps is more than 0: {}", wm.ignore_unmaps);
-                    wm.ignore_unmaps -= 1;
-                    continue;
-                }
+                    if is_mapped(wm, window) {
+                        focus_and_warp(wm, window);
+                    } else if let Some(p) = prev_focused {
+                        focus_and_warp(wm, p);
+                    }
 
-                wm.state.windows_mut().retain(|&w| w != e.window);
-                if wm.state.focused() == Some(e.window) {
-                    wm.state.set_focused(wm.state.windows().last().copied());
-                }
-
-                let intent = retile(wm);
-                map_intent(wm, intent);
-
-                wm.conn
-                    .clear_area(false, wm.screen.root, 0, 0, 0, 0)
-                    .unwrap();
-                wm.conn.flush().unwrap();
-            }
-            Event::DestroyNotify(e) => {
-                wm.state.windows_mut().retain(|&w| w != e.window);
-            }
-            Event::EnterNotify(e) => {
-                if e.mode == NotifyMode::NORMAL && e.detail != NotifyDetail::INFERIOR {
-                    focus_window(wm, e.event);
                     wm.conn.flush().unwrap();
                 }
-            }
-            Event::KeyPress(e) => {
-                println!("key: {:#?}", e);
-                let pressed = event_to_keyresult(wm, e);
+                Event::UnmapNotify(e) => {
+                    if !wm.state.windows().contains(&e.window) {
+                        continue;
+                    }
 
-                for bind in wm.keybinds.clone() {
-                    if bind.matches(&pressed) {
-                        bind.action.execute(wm);
+                    if wm.ignore_unmaps > 0 {
+                        eprintln!("ignore unmaps is more than 0: {}", wm.ignore_unmaps);
+                        wm.ignore_unmaps -= 1;
+                        continue;
+                    }
+
+                    wm.state.windows_mut().retain(|&w| w != e.window);
+                    if wm.state.focused() == Some(e.window) {
+                        wm.state.set_focused(wm.state.windows().last().copied());
+                    }
+
+                    let intent = retile(wm);
+                    map_intent(wm, intent);
+
+                    wm.conn
+                        .clear_area(false, wm.screen.root, 0, 0, 0, 0)
+                        .unwrap();
+                    wm.conn.flush().unwrap();
+                }
+                Event::DestroyNotify(e) => {
+                    wm.state.windows_mut().retain(|&w| w != e.window);
+                }
+                Event::EnterNotify(e) => {
+                    if e.mode == NotifyMode::NORMAL && e.detail != NotifyDetail::INFERIOR {
+                        focus_window(wm, e.event);
+                        wm.conn.flush().unwrap();
                     }
                 }
-                wm.conn.flush().unwrap();
+                Event::KeyPress(e) => {
+                    println!("key: {:#?}", e);
+                    let pressed = event_to_keyresult(wm, e);
+
+                    for bind in wm.keybinds.clone() {
+                        if bind.matches(&pressed) {
+                            bind.action.execute(wm);
+                        }
+                    }
+                    wm.conn.flush().unwrap();
+                }
+                e => eprintln!("event: {:?}", e),
+            },
+            Err(e) => {
+                eprintln!("X Connection Died: {e}");
+                break;
             }
-            e => eprintln!("event: {:?}", e),
         }
     }
 }
