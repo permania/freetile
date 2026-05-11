13fac11f5da67d73bb11689a25234fa3951c1938
permania <0onesixed@gmail.com>
2026-05-11 14:56:52 -0700

    feat: set up rhai config

--- src/config/reader.rs
+++ src/config/reader.rs
@@ -1,52 +1,60 @@
-use std::fs::read_to_string;
+use std::{error::Error, fs::read_to_string, io};
 
 use rhai::{Dynamic, Engine};
 
 use crate::config::layout::{Dir, WMSlot};
 
+pub fn master() -> WMSlot {
+    WMSlot::Split {
+                dir: Dir::Horizontal,
+                ratio: 0.5,
+                lhs: Box::new(WMSlot::Window),
+                rhs: Box::new(WMSlot::Drain {
+                    dir: Dir::Vertical,
+                    take: None,
+                }),
+            }
+}
+
+trait EngineSetup {
+    fn setup(&mut self);
+}
+
+impl EngineSetup for Engine {
+    fn setup(&mut self) {
+        self.register_type::<WMSlot>()
+            .register_fn("some_limit", |n: usize| Some(n))
+            .register_fn("no_limit", || None::<usize>)
+            .register_fn("h", || Dir::Horizontal)
+            .register_fn("v", || Dir::Vertical)
+            .register_fn("window", || WMSlot::Window)
+            .register_fn("stack", |dir: Dir, slots: Vec<WMSlot>| WMSlot::Stack {
+                dir,
+                slots,
+            })
+            .register_fn("drain", |dir: Dir, take: Option<usize>| WMSlot::Drain {
+                dir,
+                take,
+            })
+            .register_fn("split", |dir: Dir, ratio: f64, lhs: WMSlot, rhs: WMSlot| {
+                WMSlot::Split {
+                    dir,
+                    ratio,
+                    lhs: Box::new(lhs),
+                    rhs: Box::new(rhs),
+                }
+            })
+            .register_fn("master", || master());
+    }
+}
+
 #[allow(dead_code)]
-pub fn load_config() -> Result<(), Box<dyn std::error::Error>> {
+pub fn load_config() -> Result<WMSlot, Box<dyn Error>> {
     let mut engine = Engine::new();
-
-    engine.register_type::<WMSlot>();
-
-    engine.register_fn("some_limit", |n: usize| Some(n));
-    engine.register_fn("no_limit", || None::<usize>);
-    engine.register_fn("h", || Dir::Horizontal);
-    engine.register_fn("v", || Dir::Vertical);
-    engine.register_fn("window", || WMSlot::Window);
-    engine.register_fn("stack", |dir: Dir, slots: Vec<WMSlot>| WMSlot::Stack {
-        dir,
-        slots,
-    });
-    engine.register_fn("drain", |dir: Dir, take: Option<usize>| WMSlot::Drain {
-        dir,
-        take,
-    });
-    engine.register_fn("split", |dir: Dir, ratio: f64, lhs: WMSlot, rhs: WMSlot| {
-        WMSlot::Split {
-            dir,
-            ratio,
-            lhs: Box::new(lhs),
-            rhs: Box::new(rhs),
-        }
-    });
-    engine.register_fn("master", || WMSlot::Split {
-        dir: Dir::Horizontal,
-        ratio: 0.5,
-        lhs: Box::new(WMSlot::Window),
-        rhs: Box::new(WMSlot::Drain {
-            dir: Dir::Vertical,
-            take: None,
-        }),
-    });
+    engine.setup();
 
     let file = read_to_string("ftrc.rhai")?;
 
     let result: Dynamic = engine.eval(&file)?;
-    let layout = result.cast::<WMSlot>();
-
-    dbg!(layout);
-
-    Ok(())
+    Ok(result.cast::<WMSlot>())
 }
