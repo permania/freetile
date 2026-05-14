a109d3377abca6d5c839ad0294e56159916e60e4
permania <0onesixed@gmail.com>
2026-05-14 16:27:27 -0700

    feat: create kiplib

---
+++ kiplib/src/config/rc.rs
@@ -0,0 +1,80 @@
+use std::{collections::HashMap, fs, path::Path};
+
+#[derive(Debug)]
+pub struct Config {
+    pub sections: HashMap<String, Section>,
+}
+
+#[derive(Debug)]
+pub struct Section {
+    pub entries: HashMap<String, Value>,
+}
+
+#[derive(Debug)]
+pub enum Value {
+    Literal(String),
+    Bang(String),
+    Question(String),
+    At(String),
+}
+
+impl Value {
+    pub fn parse(s: &str) -> Value {
+        match s.chars().next() {
+            Some('!') => Value::Bang(s[1..].to_string()),
+            Some('?') => Value::Question(s[1..].to_string()),
+            Some('@') => Value::At(s[1..].to_string()),
+            _ => Value::Literal(s.to_string()),
+        }
+    }
+}
+
+pub fn read_config<T>(path: T) -> Result<Config, std::io::Error>
+where
+    T: AsRef<Path>,
+{
+    let config_string: String = fs::read_to_string(path).expect("failed to read config");
+
+    Ok(split_sections(config_string))
+}
+
+fn split_sections(config_string: String) -> Config {
+    let mut current_section: Option<String> = None;
+    let mut sections: HashMap<String, Section> = HashMap::new();
+    let trim = config_string.trim();
+
+    for line in trim.lines() {
+        if line.is_empty() {
+            continue;
+        }
+
+        if line.starts_with("    ") {
+            let line_trimmed = line.trim();
+            if let Some((ident, value)) = line_trimmed.split_once(' ') {
+                if let Some(s_name) = &current_section {
+                    if let Some(section) = sections.get_mut(s_name) {
+                        let key = ident.trim_matches('"').to_string();
+                        let value = Value::parse(value.trim().trim_matches('"'));
+
+                        section.entries.insert(key, value);
+                    }
+                }
+            } else {
+                todo!();
+            };
+            continue;
+        } else if let Some((left, _)) = line.split_once(':') {
+            let name = left.to_string();
+
+            sections.entry(name.clone()).or_insert_with(|| Section {
+                entries: HashMap::new(),
+            });
+
+            current_section = Some(name);
+
+            continue;
+        }
+    }
+
+    Config { sections }
+}
